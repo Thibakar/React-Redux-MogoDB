75fdd6a6b2c2d5a4a4fb1cf07e17132a8f756238
Thepakar M <ThepakarM@ge.com>
2021-06-29 12:59:10 +0530

    initial

---
+++ build/precache-manifest.831e381103c2002088fcdceae7fd00d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4a028bc61832736f6103b72415faf5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a568eabf4352c47d88a8",
+    "url": "/static/css/main.ad28dbc4.chunk.css"
+  },
+  {
+    "revision": "1bed8a273ccd7556a237",
+    "url": "/static/js/2.cced2f86.chunk.js"
+  },
+  {
+    "revision": "0e77ccc9007464ffd437757d6ef9c957",
+    "url": "/static/js/2.cced2f86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a568eabf4352c47d88a8",
+    "url": "/static/js/main.c6104fc9.chunk.js"
+  },
+  {
+    "revision": "75b67e50613f7dd4cc1e",
+    "url": "/static/js/runtime-main.5a1d5d0f.js"
+  }
+]);
